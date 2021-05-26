70578d1c2a01017e2a83172644a9552a39a115a3
Deepthi <deepthi.jonnagaddla@adventglobal.com>
2021-05-26 09:57:37 +0530

    Initial Commit

---
+++ SwiftyTextField/SwiftyTextField.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyTextField.h
+//  SwiftyTextField
+//
+//  Created by Deepthi on 26/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyTextField.
+FOUNDATION_EXPORT double SwiftyTextFieldVersionNumber;
+
+//! Project version string for SwiftyTextField.
+FOUNDATION_EXPORT const unsigned char SwiftyTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyTextField/PublicHeader.h>
+
+
